e72c69530e6198a886ac2baf5d7b65385d217721
Syar3005 <113889600+Syar3005@users.noreply.github.com>
2022-11-07 11:34:10 +0300

    Add files via upload

---
+++ 4.114/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("(4.114 Введите год: ");
+int year = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите номер месяца: ");
+int mounth = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите день: ");
+int dayZ = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Дата предыдущего дня: {dayZ - 1}.{mounth}.{year}");
+Console.WriteLine($"Дата следующего дня: {dayZ + 1}.{mounth}.{year}");
+
+if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+{
+    Console.WriteLine("Год високосный");
+}
+else
+{
+    Console.WriteLine("Год не високосный");
+};
